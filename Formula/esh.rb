5088171435bdec975522e8f5a2b1425830557d50
Alessio Caiazza <78752+nolith@users.noreply.github.com>
2022-09-09 09:45:39 +0000

    esh: add 0.3.2 bottle.

--- Formula/esh.rb
+++ Formula/esh.rb
@@ -5,6 +5,12 @@ class Esh < Formula
   sha256 "4103b8901c7346f5dd608f2aa272d23c259434f825e6f39b2fb642157d6a2a7c"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/nolith/stuff"
+    sha256 cellar: :any_skip_relocation, big_sur:      "bd4b0b8bcd0321aa79afdbb6bafd15c9bcb2d45828a67713063aa8902a82b914"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1c290b12139f4d1cc5a6886c60a0d404127f2ba259813b3bcc3109570e44a291"
+  end
+
   depends_on "asciidoctor" => :build
 
   def install
